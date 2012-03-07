7a06e2ac0ff22ddeb12b5fb24a12719a24eed6fd
siosio <siosio12@gmail.com>
2012-03-07 21:19:39 +0900

    change

--- src/study/FizzBuzz.kt
+++ src/study/FizzBuzz.kt
@@ -1,14 +1,14 @@
-class FizzBuzz {
-    fun main(args : Array<String>) {
-        (0..100).foreach {
-            println(
+package fizzbuzz
+
+fun main(args : Array<String>) {
+    (0..100).foreach {
+        println(
             when (#(it % 5, it % 3)) {
                 is #(0, 0) -> "FizzBuzz"
                 is #(0, *) -> "Fizz"
                 is #(*, 0) -> "Buzz"
                 else -> it
             }
-            )
-        }
+        )
     }
 }
